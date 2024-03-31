e6ef23e62d1b2ecc5751ac24b3c9f2122f12fff1
musharafaahel <musaahel@gmail.com>
2024-03-31 11:36:16 +0530

    Exception Handling

---
+++ JavaBootcamp/src/exceptionHandling/ExceptionDemo1.java
@@ -0,0 +1,22 @@
+package exceptionHandling;
+
+public class ExceptionDemo1 {
+	public static void main(String[] args) {
+		System.out.println("Before Exception");
+		try {
+			int i = 1/Integer.parseInt(args[0]); //Will throw arithmetic exception - Abnormal Condition
+		}catch(ArithmeticException e) { //Accepting the exception or Catching the exception.
+			System.out.println(e);
+			e.printStackTrace();
+		}catch (ArrayIndexOutOfBoundsException e) {
+			e.printStackTrace();
+		}catch (NumberFormatException e) {
+			e.printStackTrace();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			System.out.println("Finally Called");
+		}
+		System.out.println("After Exception");
+	}
+}
