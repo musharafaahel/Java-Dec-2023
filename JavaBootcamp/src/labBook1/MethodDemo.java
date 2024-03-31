186a78c07fd3c90097993c30107ead31366d1e13
musharafaahel <musaahel@gmail.com>
2024-03-31 07:37:28 +0530

    Methods and return type

---
+++ JavaBootcamp/src/labBook1/MethodDemo.java
@@ -0,0 +1,40 @@
+package labBook1;
+
+public class MethodDemo {
+	public static void main(String[] args) {
+		MethodDemo obj = new MethodDemo();
+		obj.metWithoutPara();
+		System.out.println(obj.metWithReturn());
+		System.out.println(obj.metWithStringReturn());
+		System.out.println(obj.metWithArrayReturn()[1]);
+		System.out.println(obj.metWithObjectReturn());
+	}
+	
+	
+	void metWithoutPara() {
+		System.out.println("method without paramenter");
+	}
+	
+	int metWithReturn() {
+		System.out.println("method with int return type");
+		return 100;
+	}
+	
+	String metWithStringReturn() {
+		System.out.println("method with String return type");
+		return "welcome";
+	}
+	
+	String[] metWithArrayReturn() {
+		System.out.println("method with Array of String return type");
+		return new String[] {"aaa", "bbb", "ccc"};
+	}
+	
+	MyClass metWithObjectReturn() {
+		System.out.println("met with object return");
+		return new MyClass();
+	}
+	
+}
+
+class MyClass {}
